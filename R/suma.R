a40a9c91d529d39b3de817053d98a4d38fbb3068
Steph0522 <shereirap@gmail.com>
2025-04-01 17:32:37 -0600

    probando función de suma

---
+++ R/suma.R
@@ -0,0 +1,16 @@
+#' Suma
+#'
+#' @param a un número 
+#' @param b otro número
+#'
+#' @returns el resultado de la suma de dos números
+#' @export
+#'
+#' @examples
+#' #' \dontrun{
+#' suma(a=1, b=2)
+#' }
+suma <- function(a,b){
+  c <- a+b
+  return(c)
+}
