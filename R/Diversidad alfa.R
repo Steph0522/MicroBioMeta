c9b306ded6e20270c9b44c0488ce4ecc5a191de4
Steph0522 <shereirap@gmail.com>
2025-04-25 15:43:42 -0600

    updating format

--- R/Diversidad alfa.R
+++ R/Diversidad alfa.R
@@ -1,18 +1,18 @@
 ##### FUNCIÓN PARA DIVERSIDAD ALFA ###
 
 
-#1. Definir la funcion 
+# 1. Definir la funcion
 #' Diversidad alfa
 #'
 #' @param otu: Otu table, en donde las columnas son las muestras y las filas ASV's. Ademas, la primer columna debe decir "OTUID", otros nombres marcara error (ejemplo OTU-ID, Sample-ID).
 #' @param metadata Mapa de características que contiene información util de las muestras.
 #' @param x Nombre de la variable del eje x
 #' @param y Nombre de la variable del eje y
-#' @param fill Nombre de la variable que separará las muestras por color 
+#' @param fill Nombre de la variable que separará las muestras por color
 #' @param V1 Variable que dividirá la figura en facets verticales
 #' @param V2 Varible que separara la figura en facets horizontales
-#' @param paleta_colores Paleta de colores blind-fliendly 
-#' @param Titulo.leyenda Título de la leyenda principal 
+#' @param paleta_colores Paleta de colores blind-fliendly
+#' @param Titulo.leyenda Título de la leyenda principal
 #' @param Titulo.figura Título principal de la figura
 #' @param Titulo.eje.y Título del eje y
 #'
@@ -21,62 +21,78 @@
 #'
 #' @examples a
 diversidad_alfa <- function(otu, metadata, x, y, fill, V1, V2, paleta_colores, Titulo.leyenda,
-                            Titulo.figura, Titulo.eje.y)
-  
-  
-  #Obtener indices de diversidad por cada orden
-  #cargar librerias
-{library(tidyverse)
-  library(hilldiv)
-  library(ggplot2)
-  library(ggpubr)
-  #El nombre de la primera columna de la otu debe ser OTUID, el nombre del metadata se cambia en el siguiente paso. 
-  otu.q0<- hilldiv::hill_div(otu,0) %>% as.data.frame() %>% mutate(orden="q=0") %>% rownames_to_column(var = "OTUID")
-  otu.q1<- hilldiv::hill_div(otu,1) %>% as.data.frame() %>% mutate(orden="q=1") %>% rownames_to_column(var = "OTUID")
-  otu.q2<- hilldiv::hill_div(otu,2) %>% as.data.frame() %>% mutate(orden="q=2") %>% rownames_to_column(var = "OTUID")
-  
-  #renombrar nombre del metadata
-  colnames(metadata)[1]<- "OTUID"
-  
-  #Unir archivos de cada orden en una sola tabla y darle nombre a la segunda columna del archivo.
-  otu.completa <- rbind(otu.q0, otu.q1, otu.q2) %>% inner_join(metadata, by = "OTUID")
-  colnames(otu.completa)[2]<- "Numero efectivo de ASV´s"
-  
-  #Especificar el orden del eje x en el que quiero la grafica final.
-  #otu.completa$variable.ordenada<- factor(otu.completa$variable.a.ordenar, levels=niveles.ordenados)
-  
-  
-  paleta_colores <- c("#F3C300","#875692","#F38400","#A1CAF1","#BE0032","#C2B280","#848482",
-                      "#008856","#E68FAC","#0067A5","#F99379","#604E97","#F6A600","#B3446C",
-                      "#DCD300","#882D17","#8DB600","#654522","#E25822","#2B3D26")
-  
-  #Figura completa
-  figura_completa<- otu.completa %>% 
-    ggpubr::ggboxplot(x = x, y = y, fill = fill)+
-    ggh4x::facet_grid2(as.formula(paste(V1,"~", V2)) , space = "fixed", scales = "free") +
-    ylab(Titulo.eje.y)+
-    xlab(NULL)+
-    scale_fill_manual(values = paleta_colores) + 
-    theme_classic()+
-    theme(legend.position = "bottom")+
-    labs(fill=Titulo.leyenda) + #Modificar titulo de la leyenda
-    ggtitle(Titulo.figura)+
-    guides(color = guide_legend(override.aes = list(size = 5))) + #tamaño del key de la primer leyenda (season)
-    theme(panel.border = element_rect(fill = "transparent", # Necesario para agregar el borde
-                                      color = "black", linewidth = 0.5),
-          strip.text.x = element_text(face = "bold", color = "white", size = 14),
-          strip.text.y = element_text(face = "bold", color = "white", size = 14),
-          strip.background.x = element_rect(fill = "#676778", linetype = "solid",
-                                            color = "black", linewidth = 0.5),
-          strip.background.y = element_rect(fill = "gray30", linetype = "solid",
-                                            color = "black", linewidth = 0.5),
-          axis.title.y = element_text(size=14, face = "bold"),
-          axis.text.y = element_text(size = 10),
-          axis.text.x = element_text(size = 10),
-          legend.title = element_text(size = 12, face = "bold"),
-          legend.text = element_text(size = 11))
-  
-  return(figura_completa)
+    Titulo.figura, Titulo.eje.y)
+    # Obtener indices de diversidad por cada orden
+# cargar librerias
+{
+    library(tidyverse)
+    library(hilldiv)
+    library(ggplot2)
+    library(ggpubr)
+    # El nombre de la primera columna de la otu debe ser OTUID, el nombre del metadata se cambia en el siguiente paso.
+    otu.q0 <- hilldiv::hill_div(otu, 0) %>%
+        as.data.frame() %>%
+        mutate(orden = "q=0") %>%
+        rownames_to_column(var = "OTUID")
+    otu.q1 <- hilldiv::hill_div(otu, 1) %>%
+        as.data.frame() %>%
+        mutate(orden = "q=1") %>%
+        rownames_to_column(var = "OTUID")
+    otu.q2 <- hilldiv::hill_div(otu, 2) %>%
+        as.data.frame() %>%
+        mutate(orden = "q=2") %>%
+        rownames_to_column(var = "OTUID")
+
+    # renombrar nombre del metadata
+    colnames(metadata)[1] <- "OTUID"
+
+    # Unir archivos de cada orden en una sola tabla y darle nombre a la segunda columna del archivo.
+    otu.completa <- rbind(otu.q0, otu.q1, otu.q2) %>% inner_join(metadata, by = "OTUID")
+    colnames(otu.completa)[2] <- "Numero efectivo de ASV´s"
+
+    # Especificar el orden del eje x en el que quiero la grafica final.
+    # otu.completa$variable.ordenada<- factor(otu.completa$variable.a.ordenar, levels=niveles.ordenados)
+
+
+    paleta_colores <- c(
+        "#F3C300", "#875692", "#F38400", "#A1CAF1", "#BE0032", "#C2B280", "#848482",
+        "#008856", "#E68FAC", "#0067A5", "#F99379", "#604E97", "#F6A600", "#B3446C",
+        "#DCD300", "#882D17", "#8DB600", "#654522", "#E25822", "#2B3D26"
+    )
+
+    # Figura completa
+    figura_completa <- otu.completa %>%
+        ggpubr::ggboxplot(x = x, y = y, fill = fill) +
+        ggh4x::facet_grid2(as.formula(paste(V1, "~", V2)), space = "fixed", scales = "free") +
+        ylab(Titulo.eje.y) +
+        xlab(NULL) +
+        scale_fill_manual(values = paleta_colores) +
+        theme_classic() +
+        theme(legend.position = "bottom") +
+        labs(fill = Titulo.leyenda) + # Modificar titulo de la leyenda
+        ggtitle(Titulo.figura) +
+        guides(color = guide_legend(override.aes = list(size = 5))) + # tamaño del key de la primer leyenda (season)
+        theme(
+            panel.border = element_rect(
+                fill = "transparent", # Necesario para agregar el borde
+                color = "black", linewidth = 0.5
+            ),
+            strip.text.x = element_text(face = "bold", color = "white", size = 14),
+            strip.text.y = element_text(face = "bold", color = "white", size = 14),
+            strip.background.x = element_rect(
+                fill = "#676778", linetype = "solid",
+                color = "black", linewidth = 0.5
+            ),
+            strip.background.y = element_rect(
+                fill = "gray30", linetype = "solid",
+                color = "black", linewidth = 0.5
+            ),
+            axis.title.y = element_text(size = 14, face = "bold"),
+            axis.text.y = element_text(size = 10),
+            axis.text.x = element_text(size = 10),
+            legend.title = element_text(size = 12, face = "bold"),
+            legend.text = element_text(size = 11)
+        )
+
+    return(figura_completa)
 }
-
-
--- R/Effect_size_volcano_plot.R
+++ R/Effect_size_volcano_plot.R
@@ -12,101 +12,105 @@
 #' @param threshold_upper Upper threshold for effect size (x-axis).
 #' @param cond Name of the condition that appears first in `table` (used in plot labels).
 #' @param show_labels Logical. Whether to display "Higher/Lower in cond" labels (default is TRUE).
-#' 
+#'
 #' @return A `ggplot` object with the volcano plot.
 #' @export
 #'
 #' @examples a
 
-#' 
-#' 
-#' 
+#'
+#'
+#'
 effect_size_plot <- function(table,
-                             metadata,
-                             col_cond,
-                             col_inf,
-                             col_sup,
-                             threshold_lower,
-                             threshold_upper,
-                             cond,
-                             show_labels = TRUE) {
-  library(ALDEx2)
-  library(ggplot2)
-  library(scales)
-  library(ggtext)
-  
-  condiciones <- metadata[[col_cond]]
-  
-  aldex_clr <- aldex.clr(table, condiciones, mc.samples = 128, denom = "all")
-  
-  effect_size <- aldex.effect(
-    aldex_clr,
-    verbose = TRUE,
-    include.sample.summary = FALSE,
-    useMC = FALSE,
-    CI = FALSE
-  )
-  
-  KW <- aldex.kw(aldex_clr, useMC = FALSE, verbose = FALSE)
-  
-  resultado <- cbind(effect_size, KW)
-  
-  resultado$grupo <- ifelse(
-    resultado$effect <= threshold_lower,
-    "Menor en condicion 1",
-    ifelse(
-      resultado$effect >= threshold_upper,
-      "Mayor en condicion 1",
-      "Normal"
+    metadata,
+    col_cond,
+    col_inf,
+    col_sup,
+    threshold_lower,
+    threshold_upper,
+    cond,
+    show_labels = TRUE) {
+    library(ALDEx2)
+    library(ggplot2)
+    library(scales)
+    library(ggtext)
+
+    condiciones <- metadata[[col_cond]]
+
+    aldex_clr <- aldex.clr(table, condiciones, mc.samples = 128, denom = "all")
+
+    effect_size <- aldex.effect(
+        aldex_clr,
+        verbose = TRUE,
+        include.sample.summary = FALSE,
+        useMC = FALSE,
+        CI = FALSE
     )
-  )
-  
-  p <- ggplot(resultado, aes(x = effect, y = kw.ep, color = grupo)) +
-    geom_point(size = 3.5) +
-    scale_color_manual(
-      values = c(
-        "Menor en condicion 1" = col_inf,
-        "Normal" = "gray",
-        "Mayor en condicion 1" = col_sup
-      )
-    ) +
-    geom_vline(xintercept = c(threshold_lower, threshold_upper), linetype = 2, color = "black") +
-    geom_hline(yintercept = 0.05, linetype = 2, color = "black") +
-    labs(
-      x = "Effect size",
-      y = bquote(italic("p") ~ " value"),
-      color = "Grupo"
-    ) +
-    ylim(c(0, 1)) +
-    theme_classic() +
-    theme(
-      axis.text = element_text(size = 15, color = "black"),
-      axis.title = element_text(size = 15),
-      legend.text = element_text(size = 12)
-    ) +
-    theme(legend.position = "none")
-  
-  if (show_labels) {
-    p <- p +
-      geom_richtext(
-        aes(x = threshold_lower, y = 0.95,
-            label = paste("<b>Menor en", cond, "</b>")),
-        color = col_inf,
-        size = 5,
-        hjust = 1,
-        vjust = 1,
-        inherit.aes = FALSE
-      ) +
-      geom_richtext(
-        aes(x = threshold_upper, y = 0.95,
-            label = paste("<b>Mayor en", cond, "</b>")),
-        color = col_sup,
-        size = 5,
-        hjust = 0,
-        vjust = 1,
-        inherit.aes = FALSE
-      )
-  }
-  
-  return(p)
+
+    KW <- aldex.kw(aldex_clr, useMC = FALSE, verbose = FALSE)
+
+    resultado <- cbind(effect_size, KW)
+
+    resultado$grupo <- ifelse(
+        resultado$effect <= threshold_lower,
+        "Menor en condicion 1",
+        ifelse(
+            resultado$effect >= threshold_upper,
+            "Mayor en condicion 1",
+            "Normal"
+        )
+    )
+
+    p <- ggplot(resultado, aes(x = effect, y = kw.ep, color = grupo)) +
+        geom_point(size = 3.5) +
+        scale_color_manual(
+            values = c(
+                "Menor en condicion 1" = col_inf,
+                "Normal" = "gray",
+                "Mayor en condicion 1" = col_sup
+            )
+        ) +
+        geom_vline(xintercept = c(threshold_lower, threshold_upper), linetype = 2, color = "black") +
+        geom_hline(yintercept = 0.05, linetype = 2, color = "black") +
+        labs(
+            x = "Effect size",
+            y = bquote(italic("p") ~ " value"),
+            color = "Grupo"
+        ) +
+        ylim(c(0, 1)) +
+        theme_classic() +
+        theme(
+            axis.text = element_text(size = 15, color = "black"),
+            axis.title = element_text(size = 15),
+            legend.text = element_text(size = 12)
+        ) +
+        theme(legend.position = "none")
+
+    if (show_labels) {
+        p <- p +
+            geom_richtext(
+                aes(
+                    x = threshold_lower, y = 0.95,
+                    label = paste("<b>Menor en", cond, "</b>")
+                ),
+                color = col_inf,
+                size = 5,
+                hjust = 1,
+                vjust = 1,
+                inherit.aes = FALSE
+            ) +
+            geom_richtext(
+                aes(
+                    x = threshold_upper, y = 0.95,
+                    label = paste("<b>Mayor en", cond, "</b>")
+                ),
+                color = col_sup,
+                size = 5,
+                hjust = 0,
+                vjust = 1,
+                inherit.aes = FALSE
+            )
+    }
+
+    return(p)
 }
