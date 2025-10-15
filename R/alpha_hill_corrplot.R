27dbe2a6d764a07d8b330cebc0707bc9c5340262
NinaMontoya <taka29811@gmail.com>
2025-10-14 23:58:15 -0600

    updating format

--- R/alpha_hill_corrplot.R
+++ R/alpha_hill_corrplot.R
@@ -23,10 +23,12 @@
 alpha_hill_corrplot <- function(table,
                                 facet_orientation = "horizontal",
                                 plot_title = "default") {
+  
+  # --- Preparación de datos ---
   table <- table[, !colnames(table) %in% "taxonomy"]
   table <- data.frame(t(table))
   
-  # Compute Hill numbers
+  # --- Calcular Hill numbers ---
   q_data <- data.frame(
     Frequency = rowSums(table),
     q0 = hillR::hill_taxa(comm = table, q = 0),
@@ -34,72 +36,122 @@ alpha_hill_corrplot <- function(table,
     q2 = hillR::hill_taxa(comm = table, q = 2)
   )
   
-  # Define common plot parameters
+  # --- Tema base común ---
   base_theme <- ggplot2::theme_test() +
-    ggplot2::theme(legend.title = ggplot2::element_blank(), 
-                   legend.position = "none", 
-                   axis.title.x = element_text(color = "black", family = "Times New Roman", size = 12),
-                   axis.title.y = element_text(color = "black", family = "Times New Roman", size = 12),
-                   axis.text.y = element_text(colour = "black", family = "Times New Roman", size = 9),
-                   axis.text.x = element_text(colour = "black", family = "Times New Roman", size = 8))
-                   
+    ggplot2::theme(
+      legend.title = ggplot2::element_blank(), 
+      legend.position = "none", 
+      axis.title.x = ggplot2::element_text(color = "black", family = "Times New Roman", size = 12),
+      axis.title.y = ggplot2::element_text(color = "black", family = "Times New Roman", size = 12),
+      axis.text.y = ggplot2::element_text(colour = "black", family = "Times New Roman", size = 8),
+      axis.text.x = ggplot2::element_text(colour = "black", family = "Times New Roman", size = 4)
+    )
   
-  # Adjust aspect ratio if vertical
-  aspect_ratio_theme <- if (facet_orientation == "horizontal") {NULL } else { 0.5}
+  # --- Relación de aspecto ---
+  aspect_ratio_theme <- if (facet_orientation == "horizontal") NULL else 0.5
   
+  # --- Gráfico q0 ---
   q0_vs_depth <- ggpubr::ggscatter(
     q_data, x = "Frequency", y = "q0",
     xlab = "Sequencing depth (number of reads)",
     add = "reg.line", conf.int = TRUE, cor.coef = TRUE,
     add.params = list(color = "#B03A2E", fill = "#566573"),
-    cor.coeff.args = list(method = "pearson", label.x = 3, label.sep = "\n")
+    cor.coeff.args = list(
+      method = "pearson",
+      label.x = 3,
+      label.sep = "\n",
+      size = 4.5,
+      family = "Times New Roman",
+      fontface = "bold",
+      color = "black"
+    )
   ) +
     base_theme +
     ggplot2::labs(y = expression(paste(italic("q"), "=0", " (number of total features)"))) +
-    ggplot2::theme(aspect.ratio = aspect_ratio_theme )
+    ggplot2::theme(aspect.ratio = aspect_ratio_theme)
   
+  # --- Gráfico q1 ---
   q1_vs_depth <- ggpubr::ggscatter(
     q_data, x = "Frequency", y = "q1",
     xlab = "Sequencing depth (number of reads)",
     add = "reg.line", conf.int = TRUE, cor.coef = TRUE,
     add.params = list(color = "#B03A2E", fill = "#566573"),
-    cor.coeff.args = list(method = "pearson", label.x = 3, label.sep = "\n")
+    cor.coeff.args = list(
+      method = "pearson",
+      label.x = 3,
+      label.sep = "\n",
+      size = 4.5,
+      family = "Times New Roman",
+      fontface = "bold",
+      color = "black"
+    )
   ) +
     base_theme +
     ggplot2::labs(y = expression(paste(italic("q"), "=1", " (number of frequent features)"))) +
-    ggplot2::theme(aspect.ratio = aspect_ratio_theme )
+    ggplot2::theme(aspect.ratio = aspect_ratio_theme)
   
+  # --- Gráfico q2 ---
   q2_vs_depth <- ggpubr::ggscatter(
     q_data, x = "Frequency", y = "q2",
     xlab = "Sequencing depth (number of reads)",
     add = "reg.line", conf.int = TRUE, cor.coef = TRUE,
     add.params = list(color = "#B03A2E", fill = "#566573"),
-    cor.coeff.args = list(method = "pearson", label.x = 3, label.sep = "\n")
+    cor.coeff.args = list(
+      method = "pearson",
+      label.x = 3,
+      label.sep = "\n",
+      size = 4.5,
+      family = "Times New Roman",
+      fontface = "bold",
+      color = "black"
+    )
   ) +
     base_theme +
     ggplot2::labs(y = expression(paste(italic("q"), "=2", " (number of dominant features)"))) +
-    ggplot2::theme(aspect.ratio = aspect_ratio_theme )
+    ggplot2::theme(aspect.ratio = aspect_ratio_theme)
   
-  # Compose plot grid
+  # --- Componer grid de gráficos ---
   grid_plot <- if (facet_orientation == "horizontal") {
-    cowplot::plot_grid(q0_vs_depth, q1_vs_depth, q2_vs_depth, labels = c("A", "B", "C"), nrow = 1)
+    cowplot::plot_grid(
+      q0_vs_depth, q1_vs_depth, q2_vs_depth,
+      labels = c("A", "B", "C"),
+      nrow = 1,
+      label_fontfamily = "Times New Roman",
+      label_fontface = "bold",
+      label_size = 16
+    )
   } else {
-    cowplot::plot_grid(q0_vs_depth, q1_vs_depth, q2_vs_depth, labels = c("A", "B", "C"), ncol = 1)
+    cowplot::plot_grid(
+      q0_vs_depth, q1_vs_depth, q2_vs_depth,
+      labels = c("A", "B", "C"),
+      ncol = 1,
+      label_fontfamily = "Times New Roman",
+      label_fontface = "bold",
+      label_size = 16
+    )
   }
   
-  # Add title based on user input
+  # --- Título del gráfico ---
   if (is.character(plot_title)) {
     if (tolower(plot_title) == "none") {
       return(grid_plot)
     } else if (tolower(plot_title) == "default") {
       title <- cowplot::ggdraw() +
-        cowplot::draw_label("Alpha diversity vs sequencing depth", fontface = 'bold', fontfamily= 'Times New Roman')
+        cowplot::draw_label("Alpha diversity vs sequencing depth",
+                            fontface = "bold",
+                            fontfamily = "Times New Roman")
     } else {
       title <- cowplot::ggdraw() +
-        cowplot::draw_label(plot_title, fontface = 'bold', fontfamily= 'Times New Roman')
+        cowplot::draw_label(plot_title,
+                            fontface = "bold",
+                            fontfamily = "Times New Roman")
     }
     cowplot::plot_grid(title, grid_plot, ncol = 1, rel_heights = c(0.1, 1))
   } else {
     return(grid_plot)
   }
 }
+
+
+
+
