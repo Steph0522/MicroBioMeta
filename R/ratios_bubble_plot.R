2727e3fd0e6cacb1dca3a6cddf41bc83ed6eb2de
NinaMontoya <taka29811@gmail.com>
2025-10-28 21:57:14 -0600

    updating family

--- R/ratios_bubble_plot.R
+++ R/ratios_bubble_plot.R
@@ -190,11 +190,11 @@ ratio_plot <- function(table,
     theme_classic(base_size = 12) +
     coord_flip() +
     theme(
-      legend.title = ggplot2::element_text(size = 14, color = "black", family = "Times New Roman", face = "bold"),
-      legend.text = ggplot2::element_text(size = 12, color = "black", family = "Times New Roman"),
-      axis.title.x = ggplot2::element_text(size = 14, color = "black", family = "Times New Roman"),
-      axis.title.y = ggplot2::element_text(size = 14, color = "black", family = "Times New Roman"),
-      axis.text.x = ggplot2::element_text(size = 12, color = "black", family = "Times New Roman"),
-      axis.text.y = ggplot2::element_text(size = 12, color = "black", family = "Times New Roman"))
+      legend.title = ggplot2::element_text(size = 14, color = "black", family = "serif", face = "bold"),
+      legend.text = ggplot2::element_text(size = 12, color = "black", family = "serif"),
+      axis.title.x = ggplot2::element_text(size = 14, color = "black", family = "serif"),
+      axis.title.y = ggplot2::element_text(size = 14, color = "black", family = "serif"),
+      axis.text.x = ggplot2::element_text(size = 12, color = "black", family = "serif"),
+      axis.text.y = ggplot2::element_text(size = 12, color = "black", family = "serif"))
   
 }
