b5ba9fa1bf667405bbf80c45c2abf0b966dc2513
Karla <estephaniazg.130897@gmail.com>
2025-10-29 14:55:00 -0600

    volcano update

--- R/aldex_volcano_plot.R
+++ R/aldex_volcano_plot.R
@@ -64,18 +64,20 @@ aldex_volcano_plot <- function(table,
   tax_col <- grep("taxonomy|Taxonomy|taxon|Taxa|taxa|Taxon", names(table), ignore.case = TRUE)
   if(length(tax_col) != 1) stop("There is no taxonomy column in the table")
   
+  table <- column_to_rownames(table, var = colnames(table)[tax_col])
+  
   # Identificar automáticamente la columna taxonómica (última columna)
-  if (is.null(taxa)) {
-    last_col <- ncol(table)
-    taxa_colname <- colnames(table)[last_col]
-    taxa <- data.frame(
-      Feature.ID = rownames(table),
-      Taxon = table[[taxa_colname]],
-      stringsAsFactors = FALSE
-    )
+  #if (is.null(taxa)) {
+   # last_col <- ncol(table)
+    #taxa_colname <- colnames(table)[last_col]
+    #taxa <- data.frame(
+     # Feature.ID = rownames(table),
+      #Taxon = table[[taxa_colname]],
+      #stringsAsFactors = FALSE
+    #)
     # Eliminar la última columna de table para el análisis
-    table <- table[, -last_col, drop = FALSE]
-  }
+    #table <- table[, -last_col, drop = FALSE]
+  #}
   
   conditions <- metadata[[col_cond]]
   groups <- unique(conditions)
@@ -90,15 +92,15 @@ aldex_volcano_plot <- function(table,
   
   # Procesar datos taxonómicos para ambos tipos de gráficos
   processed_data <- aldex_clr %>%
-    tibble::rownames_to_column(var = "Feature.ID") %>%
-    dplyr::left_join(taxa, by = "Feature.ID") %>%
+    tibble::rownames_to_column(var = "Taxon") %>%
+   # dplyr::left_join(taxa, by = "Feature.ID") %>%
     dplyr::mutate(
       taxa = dplyr::case_when(
         stringr::str_detect(Taxon, "g__") ~ stringr::str_extract(Taxon, "(?<=g__)[^_;]+"),
         stringr::str_detect(Taxon, "f__") ~ stringr::str_extract(Taxon, "(?<=f__)[^_;]+"),
         stringr::str_detect(Taxon, "c__") ~ stringr::str_extract(Taxon, "(?<=c__)[^_;]+"),
         stringr::str_detect(Taxon, "o__") ~ stringr::str_extract(Taxon, "(?<=o__)[^_;]+"),
-        TRUE ~ Feature.ID
+        TRUE ~ Taxon
       )
     )
   
